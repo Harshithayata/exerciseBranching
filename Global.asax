8d33e9222b382e64d6770f87355ed4c43c7bc842
Harshithayata <harshitha.yata@dxc.com>
2020-09-08 11:44:51 +0530

    Adding Model Emp ,EmpContext and change web api

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExerciseBranching.MvcApplication" Language="C#" %>
---
+++ Models/EmpContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExerciseBranching.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext() : base("EmpContext")
+        {
+
+
+        }
+        public DbSet<Emp> Employees { get; set; }
+    }
+}
