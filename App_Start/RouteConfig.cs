af00c7196d9a8b6d40e8ffbf736a02c040538553
Harshithayata <70888717+Harshithayata@users.noreply.github.com>
2020-09-08 13:15:56 +0530

    Update RouteConfig.cs

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ExerciseBranching
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Emps", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Emp", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
